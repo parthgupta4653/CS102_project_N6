707ed31d3af6f4eec954ce0eabd91bcf5e49289f
NAMAN PANDYA <namanpandya@NAMANs-MacBook-Air.local>
2024-03-13 11:46:45 +0530

    commit

--- home_page.js
+++ home_page.js
@@ -2,14 +2,14 @@ var collapsed = true;
 function Sidebar() {
 if (collapsed) {
 
-document.getElementById("side_bar").style.backgroundColor = "#00498D";
+document.getElementById("side_bar").style.backgroundColor = "#0c5583";
 document.getElementById("icon-text").style.display = "inline";
 document.getElementById("icon-text1").style.display = "inline";
 document.getElementById("icon-text2").style.display = "inline";
 this.collapsed = false;
 } 
 else {
-document.getElementById("side_bar").style.backgroundColor = "#3d79bd";
+document.getElementById("side_bar").style.backgroundColor = "#2c456b";
 document.getElementById("icon-text").style.display = "none";
 document.getElementById("icon-text1").style.display = "none";
 document.getElementById("icon-text2").style.display = "none";
